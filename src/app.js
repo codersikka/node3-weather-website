3de34d63276936b2f1781cc1ea09947a1dd9b4fb
ANAS <sikka@ANASs-MacBook-Pro-567.local>
2020-06-03 10:45:58 +0530

    Added new comments

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const hbs = require('hbs')
 const geocode = require ('./utils/geocode.js')
 const forecast = require ('./utils/forecast.js')
-
+//asw
 const app = express()
 
 const publicdirectoryPath = path.join(__dirname,'../public')
